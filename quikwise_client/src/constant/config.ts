154cf45d014ee54cefeae104b6512c9be84bb07a
bankbetch <jiraphun.jrb@gmail.com>
2024-05-26 22:17:58 +0700

    feat: siteConfig

--- quikwise_client/src/constant/config.ts
+++ quikwise_client/src/constant/config.ts
@@ -1,11 +1,9 @@
 import { Noto_Sans_Thai } from 'next/font/google';
 
 export const siteConfig = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
-  description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+  title: 'Next.js + Tailwind CSS + Excalidraw + mdx',
+  description: 'Quikwise is a team workspace where knowledge and collaboration meet.',
+  url: 'https://quikwise.dev',
 };
 
 export const defaultFont = Noto_Sans_Thai({
