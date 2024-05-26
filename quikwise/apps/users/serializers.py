ebff7c573c95e726c1f27ba12d6189ec97424199
sakarin.s <53126342+sang-sakarin@users.noreply.github.com>
2024-05-26 22:54:42 +0700

    bump: package version (#2)

--- quikwise/apps/users/serializers.py
+++ quikwise/apps/users/serializers.py
@@ -1,7 +1,7 @@
 from .models import User
 from apps.commons.serializers import CRUDSerializer
 from django.contrib.auth import get_user_model
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
 
