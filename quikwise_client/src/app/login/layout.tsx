bdfe75710a92bb377afc22693a560872ecc8ebeb
bankbetch <jiraphun.jrb@gmail.com>
2024-05-27 09:11:09 +0700

    style: add layout login

---
+++ quikwise_client/src/app/login/layout.tsx
@@ -0,0 +1,30 @@
+import { Metadata } from 'next';
+import * as React from 'react';
+
+import '@/styles/globals.css';
+
+import { defaultFont, siteConfig } from '@/constant/config';
+
+export const metadata: Metadata = {
+  metadataBase: new URL(siteConfig.url),
+  title: {
+    default: siteConfig.title,
+    template: `%s | ${siteConfig.title}`,
+  },
+  description: siteConfig.description,
+  robots: { index: true, follow: true },
+  icons: {
+    icon: '/favicon/favicon.ico',
+    shortcut: '/favicon/favicon-16x16.png',
+    apple: '/favicon/apple-touch-icon.png',
+  },
+  manifest: `/favicon/site.webmanifest`,
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html className={`${defaultFont.variable}`}>
+      <body className='w-screen h-screen'>{children}</body>
+    </html>
+  );
+}
